067d87bdc4c2bf712caba590c9c01d7e5e105054
axel <belaxist@gmail.com>
2025-10-01 03:53:38 +0700

    moving repository scope for all projects

---
+++ latihan1/src/com/java/pertemuan3/tugas/model/ProductModel.java
@@ -0,0 +1,45 @@
+package com.java.pertemuan3.tugas.model;
+
+public class ProductModel {
+
+    private String code;
+    private String name;
+    private int qty;
+    private double price;
+
+    public ProductModel(String code, String name, double price, int qty) {
+        this.code = code;
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
